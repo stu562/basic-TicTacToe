ec97b5089b624e7f159564426b8dfedd413a400c
Steven Tu <stu562@gmail.com>
2019-02-25 21:48:37 -0800

    created onclick event handler for player button

--- client/board.js
+++ client/board.js
@@ -1,1 +1,19 @@
-alert('connection');
+
+var player1Button = document.getElementById('player1');
+var player2Button = document.getElementById('player2');
+var resetButton = document.getElementById('reset');
+
+
+player1Button.addEventListener('click', function(){
+  alert('click');
+  //needs to increment up to the winner defined 
+});
+player2Button.addEventListener('click', function(){
+  alert('click');
+});
+resetButton.addEventListener('click', function(){
+  alert('click');
+
+  //needs to reset the board 
+});
+
