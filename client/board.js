68365bf2a15c1792dfef1c3274ef0dc4b65510be
Steven Tu <stu562@gmail.com>
2019-02-26 11:56:10 -0800

    added working tracker functionality

--- client/board.js
+++ client/board.js
@@ -2,18 +2,30 @@
 var player1Button = document.getElementById('player1');
 var player2Button = document.getElementById('player2');
 var resetButton = document.getElementById('reset');
-
+var score = {player1:0,
+              player2:0,
+            }
 
 player1Button.addEventListener('click', function(){
-  alert('click');
-  //needs to increment up to the winner defined 
+  score.player1++;
+  //needs to increment upate with new value 
+  // var currentDisplay = document.getElementsByClassName('currentscore')[0].innerText;
+  document.getElementsByClassName('currentscore')[0].innerText = `Current Score is ${score.player1} to ${score.player2}`;
+  // console.log(currentDisplay);
 });
-player2Button.addEventListener('click', function(){
-  alert('click');
-});
-resetButton.addEventListener('click', function(){
-  alert('click');
 
+player2Button.addEventListener('click', function(){
+  score.player2++
+  document.getElementsByClassName('currentscore')[0].innerText = `Current Score is ${score.player1} to ${score.player2}`;
+
+});
+
+resetButton.addEventListener('click', function(){
+  score = { player1:0,
+    player2:0,
+  }
+  document.getElementsByClassName('currentscore')[0].innerText = `Current Score is ${score.player1} to ${score.player2}`;
+  alert('Scores were reset!');
   //needs to reset the board 
 });
 
