db63dfb358c18434e51aae245cb19fab14920473
Steven Tu <stu562@gmail.com>
2019-02-26 12:18:22 -0800

    decluttered global variable, stored into object

--- client/board.js
+++ client/board.js
@@ -1,31 +1,40 @@
+var buttons = {
+  player1Button: document.getElementById('player1'),
+  player2Button: document.getElementById('player2'),
+  resetButton: document.getElementById('reset'),
+}
 
-var player1Button = document.getElementById('player1');
-var player2Button = document.getElementById('player2');
-var resetButton = document.getElementById('reset');
 var score = {player1:0,
               player2:0,
             }
 
-player1Button.addEventListener('click', function(){
+buttons.player1Button.addEventListener('click', function(){
   score.player1++;
-  //needs to increment upate with new value 
-  // var currentDisplay = document.getElementsByClassName('currentscore')[0].innerText;
   document.getElementsByClassName('currentscore')[0].innerText = `Current Score is ${score.player1} to ${score.player2}`;
-  // console.log(currentDisplay);
+  checkWinner();
 });
 
-player2Button.addEventListener('click', function(){
-  score.player2++
+buttons.player2Button.addEventListener('click', function(){
+  score.player2++;
   document.getElementsByClassName('currentscore')[0].innerText = `Current Score is ${score.player1} to ${score.player2}`;
-
+  checkWinner();
 });
 
-resetButton.addEventListener('click', function(){
+buttons.resetButton.addEventListener('click', function(){
+  resetBoard();
+});
+
+function checkWinner(){
+  if (score.player1 === 5){
+    alert('Player 1 is the winner')
+  } else if (score.player2 === 5) {
+    alert('Player 2 is the winner')
+  }
+}
+function resetBoard(){
   score = { player1:0,
     player2:0,
   }
   document.getElementsByClassName('currentscore')[0].innerText = `Current Score is ${score.player1} to ${score.player2}`;
   alert('Scores were reset!');
-  //needs to reset the board 
-});
-
+}
