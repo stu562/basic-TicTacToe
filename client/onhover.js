e4659c67dca4bd6a954b1bc6b545f6a071fffc0a
Steven Tu <stu562@gmail.com>
2019-02-27 14:37:23 -0800

    class was added to title, js colorchanger function was refactored

--- client/onhover.js
+++ client/onhover.js
@@ -1,11 +1,3 @@
-// var tic = document.getElementsByClassName('tic')[0];
-// var tic = document.querySelector("h1");
-var ticTacSelectors = {
-  tic: document.getElementsByClassName('tic')[0],
-  tac: document.getElementsByClassName('tac')[0],
-  toe: document.getElementsByClassName('toe')[0],
-}
-
 function randomNumber(){
   return Math.floor(Math.random() * 256)
 }
@@ -14,33 +6,53 @@ function randomColor(){
   return "rgb(" + randomNumber() + "," + randomNumber() + "," + randomNumber() + ")";
 }
 
-function handlesTic(){
-  ticTacSelectors.tic.addEventListener("mouseover", function(){
-    ticTacSelectors.tic.style.color = randomColor();
-  })
-  ticTacSelectors.tic.addEventListener("mouseout", function(){
-    ticTacSelectors.tic.style.color = "black";
-  })
-}  
-function handlesTac(){
-  ticTacSelectors.tac.addEventListener("mouseover", function(){
-    ticTacSelectors.tac.style.color = randomColor();
-  })
-  ticTacSelectors.tac.addEventListener("mouseout", function(){
-    ticTacSelectors.tac.style.color = "black";
-  })
-}  
-function handlesToe(){
-  ticTacSelectors.toe.addEventListener("mouseover", function(){
-    ticTacSelectors.toe.style.color = randomColor();
-  })
-  ticTacSelectors.toe.addEventListener("mouseout", function(){
-    ticTacSelectors.toe.style.color = "black";
-  })
-}  
 
-handlesTic();
-handlesTac();
-handlesToe();
+function handlesColorChange(){
+  var titleWords = document.querySelectorAll('.colorChanger');
+  for(var i = 0; i < titleWords.length; i++){
+    titleWords[i].addEventListener("mouseover", function(){
+      this.style.color = randomColor();
+    })
+    titleWords[i].addEventListener("mouseout", function(){
+      this.style.color = "black";
+    })
+  }
+}
+handlesColorChange();
+
+
+// var ticTacSelectors = {
+//   tic: document.getElementsByClassName('tic')[0],
+//   tac: document.getElementsByClassName('tac')[0],
+//   toe: document.getElementsByClassName('toe')[0],
+// }
+// function handlesTic(){
+//   ticTacSelectors.tic.addEventListener("mouseover", function(){
+//     ticTacSelectors.tic.style.color = randomColor();
+//   })
+//   ticTacSelectors.tic.addEventListener("mouseout", function(){
+//     ticTacSelectors.tic.style.color = "black";
+//   })
+// }  
+// function handlesTac(){
+//   ticTacSelectors.tac.addEventListener("mouseover", function(){
+//     ticTacSelectors.tac.style.color = randomColor();
+//   })
+//   ticTacSelectors.tac.addEventListener("mouseout", function(){
+//     ticTacSelectors.tac.style.color = "black";
+//   })
+// }  
+// function handlesToe(){
+//   ticTacSelectors.toe.addEventListener("mouseover", function(){
+//     ticTacSelectors.toe.style.color = randomColor();
+//   })
+//   ticTacSelectors.toe.addEventListener("mouseout", function(){
+//     ticTacSelectors.toe.style.color = "black";
+//   })
+// }  
+
+// handlesTic();
+// handlesTac();
+// handlesToe();
 
 
